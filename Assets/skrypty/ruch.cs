015c0532e092449f5d909083a18b5e3e70466428
FrancuzA <149786616+FrancuzA@users.noreply.github.com>
2023-12-09 18:26:25 +0100

    indicator

--- Assets/skrypty/ruch.cs
+++ Assets/skrypty/ruch.cs
@@ -7,13 +7,13 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using static UnityEditor.Timeline.TimelinePlaybackControls;
+using Unity.Mathematics;
 
 public class ruch : MonoBehaviour
 {
     private int souls;
     public int soulsCount;
     public TextMeshProUGUI soulText;
-
     public TextMeshProUGUI soulsall;
     public TextMeshProUGUI endlvl;
     public float Playerspeed;
@@ -21,7 +21,9 @@ public class ruch : MonoBehaviour
     private float timer = 0;
     public int tipTime = 5;
     public SpriteRenderer spajt;
-
+    public Transform player;
+    public Transform indicator;
+    public GameObject triangle;
     [System.Obsolete]
     void Update()
     {
@@ -33,24 +35,65 @@ public class ruch : MonoBehaviour
         {
             transform.Translate(Vector3.left *Playerspeed);
             spajt.flipX = true;
+
+           GameObject closestObject = findclosest();
+            if (closestObject != null)
+            {
+                Rotateindicator(closestObject.transform.position);
+            }
+            else
+            {
+                triangle.SetActive(false);
+            }
         }
 
         if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(Vector3.right *Playerspeed);
             spajt.flipX = false;
+            
+
+            GameObject closestObject = findclosest();
+            if (closestObject != null)
+            {
+                Rotateindicator(closestObject.transform.position);
+            }
+            else
+            {
+                triangle.SetActive(false);
+            }
         }
 
         if (Input.GetKey(KeyCode.W))
         {
             transform.Translate(Vector3.up * Playerspeed);
+
+            GameObject closestObject = findclosest();
+            if (closestObject != null)
+            {
+                Rotateindicator(closestObject.transform.position);
+            }
+            else
+            {
+                triangle.SetActive(false);
+            }
         }
 
         if (Input.GetKey(KeyCode.S))
         {
             transform.Translate(Vector3.down * Playerspeed);
+
+            GameObject closestObject = findclosest();
+            if (closestObject != null)
+            {
+                Rotateindicator(closestObject.transform.position);
+            }
+            else
+            {
+                triangle.SetActive(false);
+            }
         }
-        Debug.Log("DUSZE: " + souls);
+        
         soulText.text = souls.ToString();
         soulsall.text = allsouls.ToString();
         if( souls == allsouls)
@@ -80,6 +123,32 @@ public class ruch : MonoBehaviour
             other.gameObject.SetActive(false);
             souls++;
         }
+    }
+
+    GameObject findclosest()
+    {
+        GameObject[] objectsWithTag = GameObject.FindGameObjectsWithTag("soul");
+        float closestDistance = Mathf.Infinity;
+        GameObject closestObject = null;
+
+        foreach (GameObject obj in objectsWithTag)
+        {
+            float distance = Vector3.Distance(player.position, obj.transform.position);
+            if (distance < closestDistance)
+            {
+                closestDistance = distance;
+                closestObject = obj;
+            }
+        }
+
+        return closestObject;
+    }
+
+    void Rotateindicator(Vector3 targetPosition)
+    {
+        Vector3 direction = targetPosition - indicator.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        indicator.rotation = Quaternion.Euler(0, 0, angle - 90f);
     }
 
 }
