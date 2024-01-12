4589a4dd2534e21ef9f0ed577024a788141d9e75
cordeliae <dominika.kas934@gmail.com>
2024-01-12 14:44:17 +0100

    chuj

--- Assets/skrypty/Menu.cs
+++ Assets/skrypty/Menu.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,15 +6,30 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+   public Animator animator;
+    public float timer = 0.0f;
+    private bool isPlayClicked = false;
+
     private void Update()
     {
-       
+        if (isPlayClicked)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0.0f)
+            {
+                SceneManager.LoadScene(1);
+            }
+        }
     }
     public void LoadLevel(int index)
     {
-        SceneManager.LoadScene(index);
+        isPlayClicked = true;
+        animator.Play("menu 0");
+      
+       
     }
-    public void ExitGame() {
+    public void ExitGame()
+    {
         Application.Quit();
     }
 
